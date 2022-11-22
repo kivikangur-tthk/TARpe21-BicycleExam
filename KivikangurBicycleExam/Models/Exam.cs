3a31c55fc5c835327875af0ea4f329c85ec93d7e
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2022-11-22 16:05:08 +0200

    Add register capability

--- KivikangurBicycleExam/Models/Exam.cs
+++ KivikangurBicycleExam/Models/Exam.cs
@@ -1,9 +1,13 @@
-﻿namespace KivikangurBicycleExam.Models
+﻿using System.ComponentModel;
+
+namespace KivikangurBicycleExam.Models
 {
 	public class Exam
 	{
 		// Id=1, Name=Toomas, TR=15, 1 ,1 ,1 ,1
 		public int Id { get; set; }
+
+		[DisplayName("Examinee name")]
 		public string ExamineeName { get; set; } = "Anonymous";
 		public int? TheoryResult { get; set; } 
 		public int? ParkingLotResult { get; set; }
