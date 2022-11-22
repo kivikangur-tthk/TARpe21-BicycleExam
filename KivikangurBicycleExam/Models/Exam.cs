6292ddf00b0768bab774db8175363833d076e252
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2022-11-22 15:22:12 +0200

    Add exam model

---
+++ KivikangurBicycleExam/Models/Exam.cs
@@ -0,0 +1,13 @@
+﻿namespace KivikangurBicycleExam.Models
+{
+	public class Exam
+	{
+		// Id=1, Name=Toomas, TR=15, 1 ,1 ,1 ,1
+		public int Id { get; set; }
+		public string ExamineeName { get; set; } = "Anonymous";
+		public int? TheoryResult { get; set; } 
+		public int? ParkingLotResult { get; set; }
+		public int? SlalomResult { get; set; }
+		public int? CircleResult { get; set; }
+	}
+}
