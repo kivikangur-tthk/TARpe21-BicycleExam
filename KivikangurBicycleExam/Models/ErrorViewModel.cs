e9ddb0c79884ece28d6e0845adff181774662691
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2022-11-22 14:06:02 +0200

    Add project files.

---
+++ KivikangurBicycleExam/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KivikangurBicycleExam.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
